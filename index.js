41ce48b0bd964869f18c069a5ad82544420b5bc2
Maiorov Aleksandr <av.maiorov@digitaldrilling.ru>
2023-04-02 20:55:37 +0500

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const TelegramApi = require('node-telegram-bot-api')
+
+const TOKEN = '6266713445:AAFHzjq8Ls-oxvfpdVKXPnGq53MoCDCJdWo'
+
+const BOT = new TelegramApi(TOKEN, { polling: true })
+
+BOT.on('message', async (msg) => {
+    await console.log(msg)
+
+    const text = msg.text
+    const chatId = msg.chat.id
+
+    if (text === '/start')
+        await BOT.sendMessage(chatId, 'Hi')
+})
